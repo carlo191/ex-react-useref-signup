35977ffe8db0d99f821bd5a078bba5ca28f3fa47
Carlo Elvisi <carloelvisi01@outlook.it>
2025-03-24 18:51:06 +0100

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import "./index.css";
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+";
 
 function App() {
   const [fullName, setfullName] = useState("");
@@ -9,6 +12,24 @@ function App() {
   const [specialization, setspecialization] = useState("");
   const [experience, setexperience] = useState("");
   const [description, setdescription] = useState("");
+  const userNameIsValid = useMemo(() => {
+    const charsvaild = username.split("").every((char) => {
+      return letters.includes(char.toLowerCase()) || numbers.includes(char);
+    });
+    return charsvaild && username.trim().length >= 6;
+  }, [username]);
+  const passwordIsValid = useMemo(() => {
+    return (
+      password.trim().length >= 8 &&
+      password.split("").some((char) => letters.includes(char.toLowerCase())) &&
+      password.split("").some((char) => numbers.includes(char)) &&
+      password.split("").some((char) => symbols.includes(char))
+    );
+  }, [password]);
+
+  const isDecpritonValid = useMemo(() => {
+    return description.trim().length >= 100 && description.trim().length < 1000;
+  }, [description]);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (
@@ -18,7 +39,10 @@ function App() {
       !specialization.trim() ||
       !experience.trim() ||
       experience <= 0 ||
-      !description.trim()
+      !description.trim() ||
+      !userNameIsValid ||
+      !passwordIsValid ||
+      !isDecpritonValid
     ) {
       alert("Compila tutti i campi");
       return;
@@ -52,6 +76,13 @@ function App() {
             value={username}
             onChange={(e) => setusername(e.target.value)}
           />
+          {username.trim() && (
+            <p style={{ color: userNameIsValid ? "green" : "red" }}>
+              {userNameIsValid
+                ? "Username valido"
+                : "deve avere almeno 6 caratteri alafanumerici"}
+            </p>
+          )}
         </label>
         <label>
           <p>password</p>
@@ -60,6 +91,13 @@ function App() {
             value={password}
             onChange={(e) => setpassword(e.target.value)}
           />
+          {password.trim() && (
+            <p style={{ color: passwordIsValid ? "green" : "red" }}>
+              {passwordIsValid
+                ? "password valida"
+                : "deve avere almeno 8 caratteri tra cui 1 lettera , 1 numero e 1 simbolo"}
+            </p>
+          )}
         </label>
         <label>
           <p>specialization</p>
@@ -87,6 +125,13 @@ function App() {
             onChange={(e) => setdescription(e.target.value)}
           />
         </label>
+        {description.trim() && (
+          <p style={{ color: isDecpritonValid ? "green" : "red" }}>
+            {isDecpritonValid
+              ? "descrizione valida"
+              : "deve avere almeno 100 caratteri e massimo 1000"}
+          </p>
+        )}
         <button type="submit">Registrati</button>
       </form>
     </>
